b9e40bc5230798c31d70829d30f796953d063fda
Eliane Regina Nicácio Mendes <elianemendes@Elianes-MacBook-Air.local>
2026-03-07 15:37:30 -0300

    feat: cria estrutura da fase 2 e arquivo gitignore para esconder configurações da API

---
+++ MovieDB/MovieDB/Data/Persistence/MovieEntity+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  MovieEntity+CoreDataClass.swift
+//  MovieDB
+//
+//  Created by Eliane Regina Nicácio Mendes on 07/03/26.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias MovieEntityCoreDataClassSet = NSSet
+
+@objc(MovieEntity)
+public class MovieEntity: NSManagedObject {
+
+}
---
+++ MovieDB/MovieDB/Data/Persistence/MovieEntity+CoreDataProperties.swift
@@ -0,0 +1,33 @@
+//
+//  MovieEntity+CoreDataProperties.swift
+//  MovieDB
+//
+//  Created by Eliane Regina Nicácio Mendes on 07/03/26.
+//
+//
+
+public import Foundation
+public import CoreData
+
+
+public typealias MovieEntityCoreDataPropertiesSet = NSSet
+
+extension MovieEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<MovieEntity> {
+        return NSFetchRequest<MovieEntity>(entityName: "MovieEntity")
+    }
+
+    @NSManaged public var backdropPath: String?
+    @NSManaged public var id: String?
+    @NSManaged public var overview: String?
+    @NSManaged public var posterPath: String?
+    @NSManaged public var releaseDate: String?
+    @NSManaged public var title: String?
+    @NSManaged public var voteAverage: Double
+
+}
+
+extension MovieEntity : Identifiable {
+
+}
